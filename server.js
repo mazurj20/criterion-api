0e00e13743dd7570a31c59ff03068a2d5e71d3fa
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-09-08 16:43:33 -0400

    server connected

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 //middleware
-
-//db config
-
+app.use(express.json());
+//db config 05pb4PAlAZokfJvS
+const connection_url =
+  "mongodb+srv://admin:fV2fqzXFIFr1HfDs@cluster0.or87z.mongodb.net/criterion?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 //api route
-
+app.get("/", (req, res) => res.status(200).send("yall hear me?"));
 //listener
 app.listen(port, () => console.log(`listening on ${port}`));
